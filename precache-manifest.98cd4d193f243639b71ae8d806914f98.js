50cb4c15500550a5c7a14635740ba71b69ea226d
Muhammad Umair Abid <umairmalik0401@gmail.com>
2022-02-16 00:42:03 +0500

    Updates

---
+++ precache-manifest.98cd4d193f243639b71ae8d806914f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7c757195470f2945aaba91df91bf5f",
+    "url": "/React-Course/index.html"
+  },
+  {
+    "revision": "46907b8a3cb3df8b9863",
+    "url": "/React-Course/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/React-Course/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "03acf47b1c933fdd5274",
+    "url": "/React-Course/static/js/3.0ad1fd9d.chunk.js"
+  },
+  {
+    "revision": "46907b8a3cb3df8b9863",
+    "url": "/React-Course/static/js/main.66ee4b3f.chunk.js"
+  },
+  {
+    "revision": "3e753a56fd719a23280d",
+    "url": "/React-Course/static/js/runtime~main.cb75bc85.js"
+  }
+]);
